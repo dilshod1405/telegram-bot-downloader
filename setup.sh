7650d1a8053260c34254ee9a5b4158ce59b632b5
Dilshod <111531709+dilshod1405@users.noreply.github.com>
2024-12-07 21:25:28 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
-pip install --upgrade pip
-pip install -r requirements.txt
+#!/bin/bash
+# Install Python dependencies
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
