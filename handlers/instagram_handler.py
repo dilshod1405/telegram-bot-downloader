66bba41e1ea406f7fbdd7d0a938061df9a88f987
Dilshod <111531709+dilshod1405@users.noreply.github.com>
2024-12-13 23:20:53 +0900

    Update instagram_handler.py

--- handlers/instagram_handler.py
+++ handlers/instagram_handler.py
@@ -2,6 +2,7 @@ import os
 import instaloader
 from aiogram import types, Router, F
 from aiogram.types import InputFile
+from moviepy.editor import VideoFileClip
 
 router = Router()  # Create router for Instagram handler
 
@@ -31,21 +32,22 @@ async def instagram_handler(message: types.Message):
 
         # Determine if media is a video or a photo
         media_file_path = f"downloads/{post.shortcode}.mp4"
-        media_file_path_audio = f"downloads/{post.shortcode}.mp3"
         
         # Check size of media file
-        if os.path.getsize(media_file_path and media_file_path_audio) > 50 * 1024 * 1024:
+        if os.path.getsize(media_file_path) > 50 * 1024 * 1024:
             await message.reply("Videoning hajmi 50 MB dan oshmasligi kerak ❗️")
-        elif os.path.exists(media_file_path) and os.path.getsize(media_file_path and media_file_path_audio) <= 50 * 1024 * 1024:
+        elif os.path.exists(media_file_path) and os.path.getsize(media_file_path) <= 50 * 1024 * 1024:
             with open(media_file_path, 'rb') as f:
                 media_file = MediaFile(f.name)
+                audio_file = os.path.splitext(media_file_path)[0] + ".mp3"
+                clip = VideoFileClip(media_file_path)
+                clip.audio.write_audiofile(audio_file)
                 await message.reply_video(media_file, caption="🎥 Marhamat buyurtmangiz tayyor ✅")
-            with open(media_file_path_audio, 'rb') as f:
-                media_file = MediaFile(f.name)
-                await message.reply_audio(media_file_audio, caption="Marhamat buyurtmangiz tayyor ✅")
+                await message.reply_audio(audio_file)
+
                 
             # Delete the downloaded media file
-            formats = ['.mp4', '.jpg', '.png', '.txt']
+            formats = ['.mp4', '.jpg', '.png', '.txt', '.mp3']
             for format in formats:
                 file_path = f"downloads/{post.shortcode}{format}"
                 if os.path.exists(file_path):
